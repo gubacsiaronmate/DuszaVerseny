d7065312d4908d64782ff74e89fe008170876147
aronmategubacsi <21.gubacsi.aron.mate@mechwart.com>
2024-11-08 21:18:05 +0100

    exposed dependency imported

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,15 @@ android {
 }
 
 dependencies {
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.crypt)
+    implementation(libs.exposed.dao)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.jodatime)
+    implementation(libs.exposed.java.time)
+    implementation(libs.exposed.kotlin.datetime)
+    implementation(libs.exposed.json)
+    implementation(libs.exposed.money)
     implementation(libs.androidx.runtime.livedata)
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.kotlinx.coroutines.core)
