40921db637e399d76d89814ce9a06feb3d050947
aronmategubacsi <21.gubacsi.aron.mate@mechwart.com>
2024-11-09 00:04:54 +0100

    it aint going well

--- app/src/main/java/com/gubo/duszaverseny/MainActivity.kt
+++ app/src/main/java/com/gubo/duszaverseny/MainActivity.kt
@@ -3,10 +3,13 @@ package com.gubo.duszaverseny
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.gubo.duszaverseny.database.configureDatabase
 import com.gubo.duszaverseny.databinding.ActivityMainBinding
+import com.gubo.duszaverseny.mail.EmailSenderService
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -21,6 +24,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.startButton.setOnClickListener {
+            rememberCoroutineScope().launch {
+                EmailSenderService(
+                    "g.aronmate99@gmail.com",
+                    "Nigga",
+                    "emailTemplate",
+                    mapOf("code" to 569465)
+                ).sendEmail()
+            }
         }
     }
 }
