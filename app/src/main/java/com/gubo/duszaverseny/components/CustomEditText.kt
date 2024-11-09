7210228f91f418918e3bc32e20c8cfcaa5ff8278
xauloq <xauloq@gmail.com>
2024-11-09 12:44:55 +0100

    margin for the szervezo register

--- app/src/main/java/com/gubo/duszaverseny/components/CustomEditText.kt
+++ app/src/main/java/com/gubo/duszaverseny/components/CustomEditText.kt
@@ -9,13 +9,6 @@ import com.gubo.duszaverseny.R
 class CustomEditText(context: Context, attrs: AttributeSet):
     androidx.appcompat.widget.AppCompatEditText(context, attrs) {
     init {
-        val params = LayoutParams(
-            LayoutParams.WRAP_CONTENT,
-            LayoutParams.WRAP_CONTENT
-        ).apply {
-            setMargins(0, 40, 0, 0)
-        }
-        layoutParams = params
         setPadding(0, 16, 0, 16)
         background = AppCompatResources.getDrawable(context, R.drawable.rounded_blue_button)
         inputType = android.text.InputType.TYPE_CLASS_TEXT
