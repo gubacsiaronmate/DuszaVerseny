8230328d802e303560459ed40cb29e3c588f431c
aronmategubacsi <21.gubacsi.aron.mate@mechwart.com>
2024-11-09 17:39:34 +0100

    activities added

--- app/src/main/java/com/gubo/duszaverseny/SzervezoiLoginActivity.kt
+++ app/src/main/java/com/gubo/duszaverseny/SzervezoiLoginActivity.kt
@@ -5,22 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.gubo.duszaverseny.database.configureDatabase
-import com.gubo.duszaverseny.databinding.ActivityMainBinding
+import com.gubo.duszaverseny.databinding.ActivitySzervezoiLoginBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class SzervezoiLoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySzervezoiLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivitySzervezoiLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        configureDatabase()
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
 }
--- app/src/main/java/com/gubo/duszaverseny/VersenyzoiLoginActivity.kt
+++ app/src/main/java/com/gubo/duszaverseny/VersenyzoiLoginActivity.kt
@@ -5,18 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.gubo.duszaverseny.database.configureDatabase
-import com.gubo.duszaverseny.databinding.ActivityMainBinding
+import com.gubo.duszaverseny.databinding.ActivityEmailVerifyBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class VersenyzoiLoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEmailVerifyBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityEmailVerifyBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        configureDatabase()
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
